61178a80ce41dab3ff61d6a87ce891f188c00bbc
Val Melamed <valo@vtmelamed.com>
2026-06-21 20:43:20 -0400

    chore: update workflow names and descriptions for clarity; add common package references and usings

--- templates/AddNewPackage/content/benchmarks/MyPackage.Benchmarks/usings.cs
+++ templates/AddNewPackage/content/benchmarks/MyPackage.Benchmarks/usings.cs
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2025-2026 Val Melamed
 
-global using System.Diagnostics.CodeAnalysis;
-global using System.Runtime.Serialization;
-
-global using BenchmarkDotNet.Attributes;
-
 global using vm2.MyPackage;
 
 global using static vm2.MyPackage.MyPackageApi;
--- templates/AddNewPackage/content/tests/MyPackage.Tests/usings.cs
+++ templates/AddNewPackage/content/tests/MyPackage.Tests/usings.cs
@@ -1,13 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2025-2026 Val Melamed
 
-global using System.Diagnostics.CodeAnalysis;
-global using System.Runtime.Serialization;
-global using System.Text;
-
 global using Xunit.Abstractions;
-
-global using vm2.TestUtilities;
 
 global using static vm2.TestUtilities.TestUtilities;
 
